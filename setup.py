c256d881ce1ab95206a869b29e394623cb37e704
SohamD34 <deshmukh.2@iitj.ac.in>
2024-07-07 18:43:12 +0530

    Setup and config files for package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='pyfileconverter',
+    packages=['pyfileconverter'],
+    version='0.3',
+    description='Convert .py files runnable in vscode to jupyter .ipynb notebooks and vice versa',
+    author='Soham Deshmukh',
+    license='MIT',
+    url='https://github.com/SohamD34/PyFileConverter',
+    keywords=['vscode', 'jupyter', 'convert', 'ipynb', 'py'],
+    classifiers=[],
+)
